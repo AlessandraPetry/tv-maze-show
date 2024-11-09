f5bd7f572bbf2276c232599293e6f0001b39ba26
Alessandra Petry <apetry@Alessandras-MacBook-Pro.local>
2024-11-09 17:11:01 +0100

    feat(UI): Create Header and Footer components

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <header className="bg-black text-white py-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-xl md:text-3xl">
+          <p className="font-bold">The Powerpuff Girls</p>
+        </div>
+
+        <div className="text-lg">
+          <p className="font-medium">Alessandra Petry</p>
+        </div>
+      </div>
+    </header>
+  );
+}
