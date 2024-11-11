8919f382ba481b5c579ff0feca81a78aff93fd7b
Alessandra Petry <apetry@Alessandras-MacBook-Pro.local>
2024-11-11 13:49:27 +0100

    fix: remove dark mode styles and add alt on images

--- src/components/CardInfos.tsx
+++ src/components/CardInfos.tsx
@@ -11,9 +11,9 @@ export const CardInfos = ({ description, imageSrc, title }: CardProps) => {
   return (
     <div className="flex flex-col items-center bg-white border-l border-t border-[#efefef] rounded-3xl shadow-[2px_2px_40px_#dadada] p-16 w-full lg:flex-row lg:items-start">
       <Image
-        className="dark:invert rounded-xl"
+        className="rounded-xl"
         src={imageSrc || noImage}
-        alt=""
+        alt={imageSrc ? `${title} - image` : "no image available"}
         width={200}
         height={200}
       />
